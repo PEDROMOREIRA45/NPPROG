88949f27a309c56a682c43b12e2b17702390d6fc
PEDROMOREIRA45 <127024058+PEDROMOREIRA45@users.noreply.github.com>
2023-04-04 17:50:18 +0100

    TP04

---
+++ src/PPROG_TP04/figuras_comparator/Figura.java
@@ -0,0 +1,33 @@
+package PPROG_TP04.figuras_comparator;
+
+public abstract class Figura {
+
+    private String cor;
+
+    private static final String COR_POR_OMISSAO = "cinzento";
+
+    public Figura(String cor) {
+        this.cor = cor;
+    }
+
+    public Figura() {
+        cor=COR_POR_OMISSAO;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Cor: %s", cor);
+    }
+
+    public abstract double calcularArea();
+
+}
+
